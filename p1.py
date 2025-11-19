8db02c8022c61e77c15c2ea49ae9171a92c45526
CODEMASTER-ABDULLAH-92 <abdullahpeerzada92@gmail.com>
2025-11-19 16:44:59 +0500

    Adding the First Question

---
+++ p1.py
@@ -0,0 +1,13 @@
+#  *  *  *  *  * 
+#  *  *  *  *  * 
+#  *  *  *  *  * 
+#  *  *  *  *  * 
+#  *  *  *  *  * 
+
+rows = 5
+col = 5
+for i in range(0,rows):
+    for j in range(0,col):
+        print(" * ",end="")
+    print()
+    
